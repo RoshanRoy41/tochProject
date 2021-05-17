862a8811f65aed2f84147444b5e4420f5c3ff6f6
Roshan Roy <68818890+RoshanRoy41@users.noreply.github.com>
2021-05-17 20:26:55 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,9 @@
+function purple_btn(){
+	document.body.style.background="purple"
+}
+function green_btn(){
+	document.body.style.background="purple"
+}
+function yellow_btn(){
+	document.body.style.background="purple"
+}
